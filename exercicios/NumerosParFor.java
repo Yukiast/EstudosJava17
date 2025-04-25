f63a725b0947edd97dc40428d83b3c9a88e2e51b
Yukiast <thiagoyukiakitaaya01@gmail.com>
2025-04-25 12:30:09 -0300

    about for

---
+++ exercicios/NumerosParFor.java
@@ -0,0 +1,23 @@
+/*numeros pares que estao no intervalo de 1 a 50
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+public class NumerosParFor {
+    
+    
+    public static void main(String[] args) {
+        for(int i= 0; i <= 50 ;i++  ){
+           if (i%2 == 0) {
+                System.out.println( i);
+            
+           }
+
+        }
+        
+    } 
+}
