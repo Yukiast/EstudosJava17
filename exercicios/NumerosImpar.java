cca9ff2880c6e9885b74ca2256d245df7737d1c2
Yukiast <thiagoyukiakitaaya01@gmail.com>
2025-04-25 13:27:49 -0300

    about for

---
+++ exercicios/NumerosImpar.java
@@ -0,0 +1,27 @@
+/*faça um programa que calcule a soma de todos os numeros impares que sao multiplos de 3 e que se encontram no intevalo de 1 a 500 */
+
+
+public class NumerosImpar {
+    public static void main(String[] args) {
+        
+        for(int i =0; i <= 500 ; i++){
+            if (i%3 == 0 && i% 2 > 0){
+                int divisiveis = i;
+               
+            }
+            for(int a =0; a <= 500 ; a++){
+                if (a%2>0 && a% 3 > 0){
+                    int impar = a;
+                   System.out.println(a);
+                }
+              
+            
+            }
+        }
+    }
+}
+
+
+
+    
+
