0cec359197d1964af85ab1c101d11835f8c47ef6
Yukiast <thiagoyukiakitaaya01@gmail.com>
2025-04-25 11:27:08 -0300

    exercicio de lançamento

--- exercicios/fatorial.java
+++ exercicios/fatorial.java
@@ -5,7 +5,77 @@ import java.util.Scanner;
  */
 
 
-
 public class fatorial {
+
+
+/*public static void pausa(){
+        try{ Thread.sleep(1000);
+
+        }catch(InterruptedException e){
+            System.out.println("contagem interrompida.");
+            Thread.currentThread().interrupt();
+        }
+
     
+
+}*/
+
+
+
+
+
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);    
+        System.out.println("Para começar a contagem digite: 1");
+        
+        int comando = scanner.nextInt();
+    
+        if (comando == 1) {
+
+
+            System.out.println("Iniciando a contagem:");
+                try{ Thread.sleep(1000);
+
+                }catch(InterruptedException e){
+                    System.out.println("contagem interrompida.");
+                    Thread.currentThread().interrupt();
+                }
+
+                for(int i = 10 ; i >= 0 ; i--){
+                
+                    System.out.println(i);
+
+                try{ Thread.sleep(1000);
+
+                }catch(InterruptedException e){
+                    System.out.println("contagem interrompida.");
+                    Thread.currentThread().interrupt();
+                }
+                
+        }   
+                System.out.println("Lançado!!!");
+            
+            }else{
+                
+                System.out.println("O comando foi invalido.");
+            }
+                   
+            
+    }
+
+
+
+
+
+
+    
+    
+        
 }
+    
+        
+    
+    
+
--- fundamentosJava/EstudosFor.java
+++ fundamentosJava/EstudosFor.java
@@ -15,7 +15,7 @@ public class EstudosFor {
 
     public static void main (String[] args){
 
-        for(int i = 0; i < 10; i++){
+        for(int i = 10 ; i == 0 ; i--){
             System.out.println("o numero é "+ i);
         }
 
